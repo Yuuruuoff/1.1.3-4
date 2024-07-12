598311c4759fdd76699966f1b3d882a5358f8b47
Yuuruu <uriklab@gmail.com>
2024-07-12 12:05:18 +0300

    Singleton pattern connection to postgres

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+
+        Connection connection = Util.getInstance().getSource();
         // реализуйте алгоритм здесь
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,23 +6,46 @@ import java.util.logging.Logger;
 
 public class Util {
 
-    private final String url = "jdbc:postgresql://localhost:5438/";
-    private final String user = "postgres";
-    private final String password = "123qwe";
+    private static final String url = "jdbc:postgresql://localhost:5438/";
 
-    public Util() {
+    private static final String user = "postgres";
 
-        try (Connection con = DriverManager.getConnection(url, user, password);
-             Statement st = con.createStatement();
-             ResultSet rs = st.executeQuery("SELECT VERSION()")) {
-            if (rs.next()) {
-                System.out.println(rs.getString(1));
-            }
+    private static final String password = "123qwe";
+
+    private static Util instance;
+
+    private final Connection source;
+
+    private Util(Connection source) {
+        this.source = source;
+    }
+
+    public static Util getInstance() {
+        if (instance == null) {
+            instance = new Util(
+                    Util.connect()
+            );
+        }
+        return instance;
+    }
+    public Connection getSource(){
+        return source;
+    }
+
+
+    private static Connection connect() {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("SELECT VERSION()");
 
         } catch (SQLException ex) {
 
             Logger lgr = Logger.getLogger(Util.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
+        return connection;
     }
 }
